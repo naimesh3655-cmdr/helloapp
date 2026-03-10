0afbcb81af76e9c7e3abbb9af52e856318930f0e
naimesh3655-cmdr <naimesh3655@gmail.com>
2026-03-11 04:31:18 +0530

    UC2: Display Hello with command line argument

--- src/com.HelloWorld/HelloWorld.java
+++ src/com.HelloWorld/HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello World");
+        if(args.length > 0)
+        {
+            System.out.println("Hello " + args[0]);
+        }
+        else
+        {
+            System.out.println("Hello User");
+        }
     }
 }
