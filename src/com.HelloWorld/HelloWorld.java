eaa330fdc022312cad61031060352065bd03919b
naimesh3655-cmdr <naimesh3655@gmail.com>
2026-03-18 08:47:46 +0530

    UC4: Display hello with multiple names

--- src/com.HelloWorld/HelloWorld.java
+++ src/com.HelloWorld/HelloWorld.java
@@ -1,15 +1,25 @@
-public class HelloApp {
+public class HelloWorld {
 
     public static void main(String[] args) {
 
-        String name;
-
-        if (args.length > 0) {
-            name = args[0];
-        } else {
-            name = "World";
+        // If no arguments → default
+        if (args.length == 0) {
+            System.out.println("Hello World");
+            return;
         }
 
-        System.out.println("Hello " + name);
+        // If names are provided
+        System.out.print("Hello ");
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i]);
+
+            // Add comma except for last element
+            if (i != args.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println(); // new line
     }
 }
